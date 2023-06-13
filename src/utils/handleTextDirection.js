9ef67e654f68e6b28f487ced317011933325f852
Timothy Los <lostimothy1985@gmail.com>
2023-06-13 14:03:31 -0700

    Updated text direction and put into separate files for code reusability

--- src/utils/handleTextDirection.js
+++ src/utils/handleTextDirection.js
@@ -6,10 +6,10 @@ function handleTextDirection() {
 
     // Remove the existing text direction class
     const htmlElement = document.documentElement;
-    htmlElement.classList.remove('text-direction');
+    htmlElement.classList.remove('text-direction', 'ltr', 'rtl', 'ttb', 'btt', 'mongolian', 'bidirectional');
 
     // Add the new text direction class dynamically
-    htmlElement.classList.add(textDirection);
+    htmlElement.classList.add('text-direction', textDirection);
 }
 
 export default handleTextDirection;
